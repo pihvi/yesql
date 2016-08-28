b7f83ebcf190391d05369556082fd7cae72afcff
Antti Mattila <antti.mattila@reaktor.fi>
2016-08-29 01:49:48 +0300

    Support pg-prepared

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-###Read named SQL statements from .sql files
+### Read named SQL statements from .sql files
 
 Put your statements in a .sql file and name them with a comment above.
 e.g. `/myproject/sql/pokemon.sql`
@@ -15,6 +15,7 @@ INSERT INTO pokemon(name, price)
 Use them in code by giving the directory where .sql files(s) are
 ```javascript
 var sql = require('yesql')('/myproject/sql/')
+var db = new sqlite3.Database('/myproject/sql/db.sqlite3')
 
 db.all(sql.getPokemon, 1337, function(err, rows) {...})
 
@@ -23,3 +24,18 @@ db
   .run({name: 'pikachu', price: 99}, function(err) {...}
 ```
 
+Prepared statements for node-postgres (pg) are supported
+```sql
+-- getPokemon
+SELECT * from pokemon WHERE id = ${id};
+```
+```javascript
+var sql = require('yesql')('/myproject/sql/', {pg: true})
+
+client.query(sql.getPokemon({id: 5}), function(err, result) {...})
+```
+
+#### Changelog
+
+##### 2.6.0
+- Support pg prepared statements
--- yesql.js
+++ yesql.js
@@ -1,6 +1,8 @@
 var fs = require('fs')
 
-module.exports = function readSqlFiles(dir) {
+module.exports.pgPreparedStatement = pg
+module.exports = function readSqlFiles(dir, options) {
+  var opts = options ? options : {pg: false}
   return fs.readdirSync(dir).filter(function(file) {
     return file.endsWith('.sql')
   }).map(function(file) {
@@ -13,9 +15,26 @@ module.exports = function readSqlFiles(dir) {
     value.content.split('\n\n').forEach(function(sql) {
       if (sql.startsWith('-- ')) {
         var sqlName = sql.split('\n')[0].substring(2).trim()
-        acc[sqlName] = sql
+        acc[sqlName] = opts.pg ? pg(sql) : sql
       }
     })
     return acc
   }, {})
+}
+
+function pg(query) {
+  return function(data) {
+    var values = []
+    return {
+      text: query.replace(/\${(.+?)}/g, function(_, key) {
+        if (key in data) {
+          values.push(data[key])
+          return '$' + values.length
+        } else {
+          throw new Error('Missing value for statement.\n' + key + ' not provided for statement:\n' + query + '\nthis was provided:\n' + JSON.stringify(data))
+        }
+      }),
+      values: values
+    }
+  }
 }
