1e3091b3910df9d69e095c236c8773cad148c9a7
Christo <christo@cryptodrunks.net>
2018-09-28 18:04:10 +0200

    Don't throw on missing trailing slash

--- yesql.js
+++ yesql.js
@@ -1,27 +1,28 @@
 var fs = require('fs')
+var path = require('path')
 
 module.exports = function readSqlFiles(dir, options) {
-  var opts = options ? options : {pg: false}
-  return fs.readdirSync(dir).filter(function(file) {
-    return file.endsWith('.sql')
-  }).map(function(file) {
-    return {
-      name: file,
-      content: fs
-        .readFileSync(dir + file, 'utf8')
-        .replace(/\r\n/g, '\n')
-    }
-  }).reduce(function(acc, value) {
-    acc[value.name] = value.content
-    value.content.split('\n\n').forEach(function(sql) {
-      if (sql.startsWith('-- ')) {
-        var sqlName = sql.split('\n')[0].substring(2).trim()
-        acc[sqlName] = opts.type ? module.exports[opts.type](sql) : sql
+    var opts = options ? options : {pg: false}
+    return fs.readdirSync(dir).filter(function(file) {
+      return file.endsWith('.sql')
+    }).map(function(file) {
+      return {
+        name: file,
+        content: fs
+          .readFileSync(path.resolve(dir, file), 'utf8')
+          .replace(/\r\n/g, '\n')
       }
-    })
-    return acc
-  }, {})
-}
+    }).reduce(function(acc, value) {
+      acc[value.name] = value.content
+      value.content.split('\n\n').forEach(function(sql) {
+        if (sql.startsWith('-- ')) {
+          var sqlName = sql.split('\n')[0].substring(2).trim()
+          acc[sqlName] = opts.type ? module.exports[opts.type](sql) : sql
+        }
+      })
+      return acc
+    }, {})
+  }
 module.exports.pg = pg
 module.exports.mysql = mysql
 
